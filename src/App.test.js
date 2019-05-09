1848878aa280d56766304740d77e4f63409ece52
Aayush Dixit <dixitaayush8@gmail.com>
2019-05-09 01:13:20 -0700

    fixed a bug

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Employee from './employees/DepartmentInfo';
 
 it('renders without crashing', () => {
-  const router = document.createElement('div');
+  const div = document.createElement('div');
   ReactDOM.render(<div />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
