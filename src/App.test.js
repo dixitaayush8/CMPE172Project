420a8a4dcc4642411adf946beb5e3164ded92109
Aayush Dixit <dixitaayush8@gmail.com>
2019-05-09 01:11:09 -0700

    Added unit test for department analytics

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import Employee from './employees/DepartmentInfo';
 
 it('renders without crashing', () => {
-  const router = document.createElement('TableRow');
-  ReactDOM.render(<TableRow />, tableRow);
-  ReactDOM.unmountComponentAtNode(tableRow);
+  const router = document.createElement('div');
+  ReactDOM.render(<div />, div);
+  ReactDOM.unmountComponentAtNode(div);
 });
